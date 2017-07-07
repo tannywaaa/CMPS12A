50a2a5aae579438bcdec63643190d1273d0abb4f
tannywaaa <wangtangni96@gmail.com>
2017-07-06 22:51:55 -0700

    lab1

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// HelloWorld.java
+// Tangni Wang
+// twang63
+// lab1
+//It prints out ""Hello, my name is Tangni Wang"
+class HelloWorld{
+   public static void main(String[] args){
+      System.out.println("Hello, my name is Tangni Wang!");
+   } 
+}
