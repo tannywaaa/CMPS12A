312bfa4a280a99caeec9d0c9f25896604737cbdc
tannywaaa <wangtangni96@gmail.com>
2017-07-06 23:10:31 -0700

    Create LetterHome.java

---
+++ lab5/LetterHome.java
@@ -0,0 +1,112 @@
+//----------------------------------------------------------- 
+//// LetterHome.java
+//// Tangni Wang
+//// 1486575
+//// Lab5
+//// This program write a letter to home for college student by easily just answe each question. 
+////-----------------------------------------------------------
+
+import java.util.Scanner;
+import java.io.File;
+
+class LetterHome{
+
+   static final int MAX_CODE = 5;
+
+   public static void main(String[] args) throws Exception{
+      Scanner in = new Scanner(new File(args[0]));
+      String phrase;
+      int sentenceCode, modifierCode;
+
+      // print heading
+      System.out.println("Dear Mom and Dad:\n");
+
+      // print encoded sentences
+      while( in.hasNext() ){
+
+         // read a pair of sentence and modifier codes
+         sentenceCode = in.nextInt();
+         modifierCode = in.nextInt();
+
+         // make sure the sentenceCode is within range
+         //if( (sentenceCode < 1) || (sentenceCode >= MAX_CODE) ) {
+         //The error her is the second part. The range of the cases is between 1 to 5 so the case should be either less than one or greater than five. Here the program are taking all cases less than or equal to 5 into a invalid case. Plus the case when sentenceCode is 5 should not be included to the invalid cases.
+         if( (sentenceCode < 1) || (sentenceCode > MAX_CODE) ) {
+            System.out.println(sentenceCode + " is not a valid sentence code");
+            continue;
+         }
+
+         // print the appropriate sentence with modifier
+         if( sentenceCode == 1 ){
+            if( modifierCode == 1 ){
+               phrase = "great";
+            }else if( modifierCode == 2 ){
+               phrase = "ok";
+            }else{
+               phrase = "ERROR";
+            }
+            System.out.println("My classes are going " + phrase + ".");
+
+         }else if( sentenceCode == 2 ){
+            phrase = weatherModifier(modifierCode);
+            System.out.println("The weather here has been " + phrase + ".");
+
+         }else if( sentenceCode == 3 ){
+            if( modifierCode == 1 ){
+               phrase = "after the quarter ends";
+            }else if( modifierCode == 2 ){
+               phrase = "in a few weeks";
+            }else if( modifierCode == 3 ){
+               phrase = "next weekend";
+            }else{
+               phrase = "ERROR";
+            }
+            System.out.println("I plan to come home for a visit " + phrase + ".");
+
+         }else if( sentenceCode == 4 ){
+            System.out.println("Do you think you could send me $" + modifierCode + "?");
+            System.out.println("I have to buy another book for one of my classes.");
+
+         }else if( sentenceCode == 5 ){
+            if( modifierCode == 1 ){
+               phrase = "cookies";
+            }else if( modifierCode == 2 ){
+               phrase = "stuff";
+		//there is an extra semicolumn between "else" and "if".
+            //}else; if( modifierCode == 3 ){
+            }else if(modifierCode == 3){
+               phrase = "money";
+            }else{
+               phrase = "ERROR";
+            }
+            System.out.println("Thanks for the " + phrase + " you sent.");
+         }
+      }
+   }
+
+
+   // weatherModifier()
+   // returns the correct word for a weather sentence
+   static String weatherModifier(int m) {
+      String word=null;
+
+      // select weather modifier
+      if(m == 1)
+         word = "great";
+      //if(m == 2);
+      //There is an extra semicolumn after the if statement.
+      if(m == 2)
+         word = "foggy";
+      if(m == 3)
+         word = "hot";
+      if(m == 4)
+         word = "cold";
+      if(m == 5)
+         word = "variable";
+      if( m<1 || m>5)
+         word = "ERROR";
+
+      return word;
+   }
+
+}
